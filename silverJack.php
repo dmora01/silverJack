de827ad2ec77e66a3b79d7c014f3f6a137a39030
Daniel Mora <dmora@csumb.edu>
2016-09-20 20:41:31 +0000

    added function and other variables

--- silverJack.php
+++ silverJack.php
@@ -3,22 +3,117 @@ $deck = array();
 $usedCards = array();
 $suits = array("clubs", "diamonds", "hearts", "spades");
 $players = array("Player1", "Player2", "Player3", "Player4");
-function displayRandomCard(){
-  
+$player1 = array();
+$player2 = array();
+$player3 = array();
+$user = array();
+
+function displayRandomCard()
+{
   for ($i = 1; $i <= 52; $i++) 
   {
       $deck[] = $i;
   }
   
   shuffle($deck);
- 
-  
-  $randomSuitIndex = rand(0,3);
-  $randomSuit = $suits[$randomSuitIndex];      
-  
+  getCards();
 }
-function getHand(){
-	
+function getCards()
+{
+    $temp = rand(0, 3);
+    $hold = rand(1, 13);
+    
+    for ($i = 0; $i < 4; $i++)
+    {
+        foreach ($usedCards as $x => $x_value)
+	   {
+	       if ($player1[$x] != $x_value && array_key_exists($x, $player1) != true)
+	       {
+	           $player1[$suits[$temp]] = $hold;
+	           $usedCards[$suits[$temp]] = $hold;
+	       }
+	   }
+	   
+	    $temp = rand(0, 3);
+        $hold = rand(1, 13);
+    }
+    
+    $temp = rand(0, 3);
+    $hold = rand(1, 13);
+    
+    for ($i = 0; $i < 4; $i++)
+    {
+        foreach ($usedCards as $x => $x_value)
+	   {
+	       if ($player2[$x] != $x_value && array_key_exists($x, $player2) != true)
+	       {
+	           $player2[$suits[$temp]] = $hold;
+	           $usedCards[$suits[$temp]] = $hold;
+	       }
+	   }
+	   
+	    $temp = rand(0, 3);
+        $hold = rand(1, 13);
+    }
+    
+    $temp = rand(0, 3);
+    $hold = rand(1, 13);
+    
+    for ($i = 0; $i < 4; $i++)
+    {
+        foreach ($usedCards as $x => $x_value)
+	   {
+	       if ($player3[$x] != $x_value && array_key_exists($x, $player3) != true)
+	       {
+	           $player3[$suits[$temp]] = $hold;
+	           $usedCards[$suits[$temp]] = $hold;
+	       }
+	   }
+	   
+	    $temp = rand(0, 3);
+        $hold = rand(1, 13);
+    }
+    
+    $temp = rand(0, 3);
+    $hold = rand(1, 13);
+    
+    for ($i = 0; $i < 4; $i++)
+    {
+        foreach ($usedCards as $x => $x_value)
+	   {
+	       if ($user[$x] != $x_value && array_key_exists($x, $user) != true)
+	       {
+	           $user[$suits[$temp]] = $hold;
+	           $usedCards[$suits[$temp]] = $hold;
+	       }
+	   }
+	   
+	    $temp = rand(0, 3);
+        $hold = rand(1, 13);
+    }
+}
+function getHand($playerNum)
+{
+    $flag = false;
+    
+	if ($playerNum == 0)
+	{
+	    if (count($usedCards) != 0)
+	    {
+	        foreach ($usedCards as $x => $x_value)
+	        {
+	            if ($player1[$x] == $x_value && array_key_exists($x, $player1) == true)
+	            {
+	                $flag = true;
+	            }
+	        }
+	        
+	        if ($flag == false)
+	        {
+	            
+	        }
+	    }
+	}
 }
 function displayHand(){
 	for($i = 0; $i < 4; $i++){
