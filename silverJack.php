5f0965c7c7614e0274937e0347ecd5a717d80d17
Daniel Mora <dmora@csumb.edu>
2016-09-20 18:04:29 +0000

    more arrays

--- silverJack.php
+++ silverJack.php
@@ -1,28 +1,21 @@
 <?php
 
-function displayRandomCard() {
- 
+function displayRandomCard() 
+{
   $deck = array();
-  for ($i = 1; $i <= 52; $i++) {
-      
+  $usedCards = array();
+  
+  for ($i = 1; $i <= 52; $i++) 
+  {
       $deck[] = $i;
-      
   }
   
   shuffle($deck);
-  print_r($deck);
-
-  $card = array_pop($deck);
-  
-  echo $card . "<br />";
-  
  
   $suits = array("clubs", "diamonds", "hearts", "spades");
   $randomSuitIndex = rand(0,3);
   $randomSuit = $suits[$randomSuitIndex];      
   echo "<img src='img/cards/$randomSuit/" . rand(1,13). ".png' />";
-
-    
 }
 
 ?>
@@ -30,7 +23,7 @@ function displayRandomCard() {
 <!DOCTYPE html>
 <html>
     <head>
-        <title> </title>
+        <title>Silver Jack</title>
     </head>
     <body>
         
