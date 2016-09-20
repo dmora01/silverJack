d17d54bc52416edbd86b99baa2bafcc9e1844fb4
navlag <crgalvan@csumb.edu>
2016-09-20 17:51:34 +0000

    display random card

--- silverJack.php
+++ silverJack.php
@@ -1,20 +1,40 @@
 <?php
 
+function displayRandomCard() {
+ 
+  $deck = array();
+  for ($i = 1; $i <= 52; $i++) {
+      
+      $deck[] = $i;
+      
+  }
+  
+  shuffle($deck);
+  print_r($deck);
+
+  $card = array_pop($deck);
+  
+  echo $card . "<br />";
+  
+ 
+  $suits = array("clubs", "diamonds", "hearts", "spades");
+  $randomSuitIndex = rand(0,3);
+  $randomSuit = $suits[$randomSuitIndex];      
+  echo "<img src='img/cards/$randomSuit/" . rand(1,13). ".png' />";
+
+    
+}
+
 ?>
 
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Silver Jack</title>
-	</head>
-	
-	<main>
-		<body>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+        
+        <?=displayRandomCard() ?>
 
-		</body>
-	</main>
-	
-	<footer>
-		
-	</footer>
+    </body>
 </html>
