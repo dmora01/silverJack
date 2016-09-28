18e2ff91a6f2a4c0493046bd87df9d3643e647d6
Daniel Mora <dmora@csumb.edu>
2016-09-28 00:27:10 +0000

    Finished assigning cards

--- silverJack.php
+++ silverJack.php
@@ -19,7 +19,7 @@ function displayRandomCard()
   
 	$temp = rand(1, 52);
 	
-	for ($i = 0; $i < 4; $i++)
+	for ($i = 0; $i < 4; $i++) // Assigning cards for player 1
 	{
     	if (in_array($deck[$temp], $used) == false)
     	{
@@ -44,7 +44,7 @@ function displayRandomCard()
    print_r($player1Cards);
    echo "<br><br>";
     
-   for ($i = 0; $i < 4; $i++)
+   for ($i = 0; $i < 4; $i++) // Assigning cards for player 2
    {
     	if (in_array($deck[$temp], $used) == false)
     	{
@@ -69,7 +69,7 @@ function displayRandomCard()
     print_r($player2Cards);
     echo "<br><br>";
     
-    for ($i = 0; $i < 4; $i++)
+    for ($i = 0; $i < 4; $i++) //Assigning cards for player 3
     {
     	if (in_array($deck[$temp], $used) == false)
     	{
@@ -93,6 +93,184 @@ function displayRandomCard()
     }
     print_r($player3Cards);
     echo "<br><br>";
+    
+    for ($i = 0; $i < 4; $i++) //Assigning cards for the user
+    {
+    	if (in_array($deck[$temp], $used) == false)
+    	{
+    		$used[$usedCount] = $deck[$temp];
+    		$usedCount++;
+    		$user[$i] = $deck[$temp];
+    		$temp = rand(1, 52);
+    	}
+    	else
+    	{
+    		while (in_array($deck[$temp], $used) == true)
+    		{
+    			$temp = rand(1, 52);
+    		}
+    		
+    		$used[$usedCount] = $deck[$temp];
+    		$usedCount++;
+    		$user[$i] = $deck[$temp];
+    		$temp = rand(1, 52);
+    	}
+    }
+    print_r($user);
+    echo "<br><br>";
+    
+    $amount = 0;
+    $size = count($player1Cards);
+    
+    for ($i = 0; $i < count($player1Cards); $i++) // Counting up values to see the amount for player 1
+    {
+    	$amount += ($player1Cards[$i] % 13) + 1;
+    }
+    
+    if ($amount <= 35)
+    {
+    	while ($amount <= 35)
+    	{
+			if (in_array($deck[$temp], $used) == false)
+    		{
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player1Cards[$size] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		else
+    		{
+    			while (in_array($deck[$temp], $used) == true)
+    			{
+    				$temp = rand(1, 52);
+    			}
+    		
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player1Cards[$i] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		
+    		$amount += ($player1Cards[$size] % 13) + 1; //Used mod to find the wrap around and find the actual value
+    		$size++;
+    	}
+    }
+    echo $amount;
+    echo "<br>";
+    
+    $amount = 0;
+    $size = count($player2Cards);
+    
+    for ($i = 0; $i < count($player2Cards); $i++) //Finding total amount for player 2
+    {
+    	$amount += ($player1Cards[$i] % 13) + 1;
+    }
+    
+    if ($amount <= 35)
+    {
+    	while ($amount <= 35)
+    	{
+			if (in_array($deck[$temp], $used) == false)
+    		{
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player2Cards[$size] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		else
+    		{
+    			while (in_array($deck[$temp], $used) == true)
+    			{
+    				$temp = rand(1, 52);
+    			}
+    		
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player2Cards[$i] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		
+    		$amount += ($player2Cards[$size] % 13) + 1; //Using mod to find actual value
+    		$size++;
+    	}
+    }
+    echo $amount;
+    echo "<br>";
+    $amount = 0;
+    $size = count($player3Cards);
+    
+    for ($i = 0; $i < count($player3Cards); $i++) //Finding total vaue for player3
+    {
+    	$amount += ($player1Cards[$i] % 13) + 1;
+    }
+    
+    if ($amount <= 35)
+    {
+    	while ($amount <= 35)
+    	{
+			if (in_array($deck[$temp], $used) == false)
+    		{
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player3Cards[$size] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		else
+    		{
+    			while (in_array($deck[$temp], $used) == true)
+    			{
+    				$temp = rand(1, 52);
+    			}
+    		
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$player3Cards[$i] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		
+    		$amount += ($player3Cards[$size] % 13) + 1; //Using mod to find actual value
+    		$size++;
+    	}
+    }
+    echo $amount;
+    echo "<br>";
+    $amount = 0;
+    $size = count($user);
+    
+    for ($i = 0; $i < count($user); $i++)
+    {
+    	$amount += ($player1Cards[$i] % 13) + 1;
+    }
+    
+    if ($amount <= 35)
+    {
+    	while ($amount <= 35)
+    	{
+			if (in_array($deck[$temp], $used) == false)
+    		{
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$user[$size] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		else
+    		{
+    			while (in_array($deck[$temp], $used) == true)
+    			{
+    				$temp = rand(1, 52);
+    			}
+    		
+    			$used[$usedCount] = $deck[$temp];
+    			$usedCount++;
+    			$user[$i] = $deck[$temp];
+    			$temp = rand(1, 52);
+    		}
+    		
+    		$amount += ($user[$size] % 13) + 1;
+    		$size++;
+    	}
+    }
+    echo $amount;
 }
 function displayHand()
 {
